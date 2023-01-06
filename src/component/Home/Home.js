f7d243045299edbdb5d4785383efb1665f498db5
RyukAD <>
2023-01-06 09:42:11 +0530

    all

---
+++ src/component/Home/Home.js
@@ -0,0 +1,56 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        {/* can adjust svg in background */}
+        <div>
+            {/* for svg */}
+        </div>
+        <div>
+            {/* for upper heading */}
+            <div>
+                {/* main heading */}
+            </div>
+            <div>
+                {/* para description */}
+            </div>
+            <button>
+                {/*  */}
+            </button>
+        </div>
+        <div>
+            {/* semi circle */}
+        </div>
+        <div>
+            {/* book image */}
+        </div>
+        <div>
+            {/* home footer more, scroll to explore, insta facebook twitter */}
+        </div>
+        <div>
+            {/* testominals */}
+            <div>
+                {/* 1st */}
+            </div>
+            <div>
+                {/* 2nd */}
+            </div>
+        </div>
+        <div>
+            {/* counter for number of books sold */}
+        </div>
+        <div>
+            {/* About book */}
+            <button>
+            {/* more about book */}
+            </button>
+        </div>
+        <div>
+            {/* About author */}
+        </div>
+    </div>
+  )
+}
+
+export default Home
